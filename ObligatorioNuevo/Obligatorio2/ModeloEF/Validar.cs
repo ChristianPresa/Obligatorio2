3871e0944a7b5de05a31c982470acdbc227bcddc
ChristianPresa <ch.gp90@gmail.com>
2022-10-22 14:52:22 -0300

    se genera el sitio y tipo mensaje

--- ObligatorioNuevo/Obligatorio2/ModeloEF/Validar.cs
+++ ObligatorioNuevo/Obligatorio2/ModeloEF/Validar.cs
@@ -8,7 +8,7 @@ namespace ModeloEF
 {
     public class Validar
     {
-        public static void Mensaje(Mensaje Men)
+        public static void ValidarMensaje(Mensaje Men)
         {
             if (Men.Asunto.Trim() == "")
                 throw new Exception("El mensaje debe contener un Asuto");
@@ -18,9 +18,14 @@ namespace ModeloEF
                 throw new Exception("El mensaje debe contener almenos un destinatario.");
             if (Men.Tipo == null)
                 throw new Exception("Debe contener un tipo de mensaje.");
+            if (Men.Usuario1 == null)
+                throw new Exception("Debe tener un usuario de destino");
+            if (Men.Usuario1.Count == 0)
+                throw new Exception("Debe indicar un destinatario");
+            
         }
 
-        public static void Usuarios(Usuario Usu)
+        public static void ValidarUsuarios(Usuario Usu)
         {
             if (!(Usu.Mail.Trim().Length <= 20 && Usu.Mail.Trim() != "")) //FALTA VALIDAR @
                     throw new Exception("Debe contener contener un mail.");
@@ -30,16 +35,19 @@ namespace ModeloEF
                 throw new Exception("Debe contener un nombre de usuario de 8 caracteres.");
             if (!(System.Text.RegularExpressions.Regex.IsMatch(Usu.Pass, "[A-Za-z]{5}[0-9]{1}")))
                 throw new Exception("Las password debe contener 5 letras y 1 numero");
+            
                
         }
 
-        public static void Tipo(Tipo Tipo)
+        public static void ValidarTipo(Tipo Tipo)
         {
             if (!(System.Text.RegularExpressions.Regex.IsMatch(Tipo.CodigoTipo, "[A-Za-z]{3}")))
                 throw new Exception("El tipo debe contener 3 letras");
             if (!(Tipo.NombreTipo.Trim().Length <= 15 && Tipo.NombreTipo.Trim() != ""))
                 throw new Exception("El mensaje debe contener un nombre y debe ser menor a 15 letras."); //Misma validacion que proyecto anerior?
-            //Cambio 2
+            
+            
+
         }
     }
 }
