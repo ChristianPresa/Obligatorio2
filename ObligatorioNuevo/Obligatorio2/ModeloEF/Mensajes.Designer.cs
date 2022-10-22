67d996f74e0bb7015121a91dbf1664b4c7bc7e85
Zot1co <mathz@hotmail.es>
2022-10-22 09:52:14 -0300

    Se modifico clase ,validaciones Entity. Falta terminar.

--- ObligatorioNuevo/Obligatorio2/ModeloEF/Mensajes.Designer.cs
+++ ObligatorioNuevo/Obligatorio2/ModeloEF/Mensajes.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\usuario\Desktop\obligatorio 2\Nueva carpeta\ObligatorioNuevo\Obligatorio2\ModeloEF\Mensajes.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Mathi\Desktop\Nueva carpeta (2)\Obligatorio2\ObligatorioNuevo\Obligatorio2\ModeloEF\Mensajes.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
--- ObligatorioNuevo/Obligatorio2/ModeloEF/Validar.cs
+++ ObligatorioNuevo/Obligatorio2/ModeloEF/Validar.cs
@@ -11,7 +11,7 @@ namespace ModeloEF
         public static void Mensaje(Mensaje Men)
         {
             if (Men.Asunto.Trim() == "")
-                throw new Exception("El mensaje debe tener Asunto.");
+                throw new Exception("El mensaje debe contener un Asuto");
             if (Men.Texto.Trim() == "")
                 throw new Exception("El mensaje debe contener un Texto.");
             if (Men.Usuario1.Count >= 1)
@@ -22,22 +22,23 @@ namespace ModeloEF
 
         public static void Usuarios(Usuario Usu)
         {
-            if (Usu.Mail.Trim() == "")
-                throw new Exception("Debe contener contener un mail.");
-            if (Usu.NombreCompleto.Trim() == "")
+            if (!(Usu.Mail.Trim().Length <= 20 && Usu.Mail.Trim() != "")) //FALTA VALIDAR @
+                    throw new Exception("Debe contener contener un mail.");
+            if (Usu.NombreCompleto.Trim() == "" || Usu.NombreCompleto.Trim().Length > 30)
                 throw new Exception("Debe contener un mensaje.");
-            if (Usu.Pass.Trim() == "")
-                throw new Exception("Debe contener una password");
-            if (Usu.NombreUsuario.Trim() == "")
-                throw new Exception("Debe contener un nombre de usuario.");
+            if (!(System.Text.RegularExpressions.Regex.IsMatch(Usu.NombreUsuario, "[A-Za-z]{8}")))
+                throw new Exception("Debe contener un nombre de usuario de 8 caracteres.");
+            if (!(System.Text.RegularExpressions.Regex.IsMatch(Usu.Pass, "[A-Za-z]{5}[0-9]{1}")))
+                throw new Exception("Las password debe contener 5 letras y 1 numero");
+               
         }
 
         public static void Tipo(Tipo Tipo)
         {
-            if (Tipo.CodigoTipo.Trim().Length == 3)
+            if (!(System.Text.RegularExpressions.Regex.IsMatch(Tipo.CodigoTipo, "[A-Za-z]{3}")))
                 throw new Exception("El tipo debe contener 3 letras");
-            if (Tipo.NombreTipo.Trim() == "")
-                throw new Exception("El mensaje debe contener un nombre");
+            if (!(Tipo.NombreTipo.Trim().Length <= 15 && Tipo.NombreTipo.Trim() != ""))
+                throw new Exception("El mensaje debe contener un nombre y debe ser menor a 15 letras."); //Misma validacion que proyecto anerior?
         }
     }
 }
